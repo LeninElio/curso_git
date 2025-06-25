27b24297243603173a08d0ad501fdaeb02d4e07b
Lenin Elio <moreno_leninelio@hotmail.es>
2025-06-25 08:54:35 -0500

    Add: ejemplo de uso en main

--- merge_example/merge_example.py
+++ merge_example/merge_example.py
@@ -1,4 +1,4 @@
-from funciones import obtener_primera_letra
+from funciones import *
 
 lista_cursos = [
     'Python',
@@ -22,5 +22,12 @@ lista_cursos = [
 
 for curso in lista_cursos:
     primera_letra = obtener_primera_letra(curso)
+    ultima_letra = obtener_ultima_letra(curso)
+    letra_aleatoria = obtener_letra_aleatoria(curso)
+
     if primera_letra:
         print(f"La primera letra del curso '{curso}' es: {primera_letra}")
+    if ultima_letra:
+        print(f"La última letra del curso '{curso}' es: {ultima_letra}")
+    if letra_aleatoria:
+        print(f"Una letra aleatoria del curso '{curso}' es: {letra_aleatoria}")
